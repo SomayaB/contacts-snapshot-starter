6d06e67c0bd324b1b4b22b3dd93ce47af93ecc03
SomayaB <somaya.bounouar@gmail.com>
2017-08-24 15:17:19 -0700

    Add users table to schema

--- src/db/schema/schema.sql
+++ src/db/schema/schema.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE contacts (
   id serial,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
+);
+
+CREATE TABLE users (
+  id serial,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL
 );
